b06ef3deeb242a5b8c1fc09e6e1b73aeebdd66a0
levis <127704463+lvs29@users.noreply.github.com>
2026-06-24 16:33:36 -0300

    Update db.py

--- db.py
+++ db.py
@@ -8,10 +8,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship, Session
 DB_PATH = os.environ.get("DB_PATH", "database.db")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
